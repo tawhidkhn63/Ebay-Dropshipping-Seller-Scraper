3cfaeab0cdb0da2f7b6e4ff954f9d49aceac102c
root <root@TawhidKhanLaptop.localdomain>
2019-05-17 19:35:27 -0400

    Created google search function that returns list of other sites who sell the item

--- ds_seller_scraper.py
+++ ds_seller_scraper.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 # List of item names to search on eBay
 #item_list = ["Mountain Bike", "tv stand", "fridge", "chair", "keyboard", "rack",
@@ -20,6 +21,16 @@ def make_urls(names):
 
     # Returns the list of completed urls
     print("URL list created.")
+    return urls
+
+def google_search(item):
+    page = requests.get("http://www.google.com/search?q="+item)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    links = soup.find_all("a",href=re.compile("(?<=/url\?q=)(htt.*://.*)"))
+    urls = [re.split(":(?=http)",link["href"].replace("/url?q=",""))[0] for link in links]
+    urls = [url for url in urls if "ebay.com" not in url] 
+    urls = [url for url in urls if 'google.com' not in url] 
+    urls = [url for url in urls if 'webcache' not in url] 
     return urls
 
 # Scrapes and returns list of in demand items listed on eBay search result page
@@ -59,14 +70,23 @@ def in_demand_item_finder(urls):
     print("In demand items found.")
     return in_demand_items
 
-def other_retailer_checker(item_link):
-	
+def description_searcher(item_link):
+    other_retail_links = []
     # Downloads the eBay page for processing
     res = requests.get(item_link)
     # Raises an exception error if there's an error downloading the website
     res.raise_for_status()
     # Creates a BeautifulSoup object for HTML parsing
     soup = BeautifulSoup(res.text, 'html.parser')
+    
+
+def other_retailer_checker(item_link):
+    item_title = ((item_link.lstrip("https://www.ebay.com/itm")).split('/'))[0]
+    item_title = re.sub('-', ' ', item_title)
+    # Parses the item link to determine the item title
+   
+    print(google_search(item_title))
+    #description_searcher(item_link)
     
 
 # Opens a hot item and determines feedback score of seller
@@ -88,7 +108,7 @@ def eBay_item_checker(hot_items):
         feedback_score = feedback_score['title'].lstrip("feedback score:")
 
 
-        if((int(feedback_score) > 100) and (int(feedback_score) > 10000)):
+        if((int(feedback_score) > 100) and (int(feedback_score) < 10000)):
             other_retailer_checker(item_link)
             #add_seller(item_link)
         print(feedback_score)
