dc09e08da0cfda8c809433ab4e48146e4cf12d49
root <root@TawhidKhanLaptop.localdomain>
2019-05-19 19:41:37 -0400

    Created Walmart price checker, and made sure items do not appear more than once in hot items list. Working on Amazon price checker

--- ds_seller_scraper.py
+++ ds_seller_scraper.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import sys 
+from amazon.api import AmazonAPI
 
 # List of item names to search on eBay
 #item_list = ["Mountain Bike", "tv stand", "fridge", "chair", "keyboard", "rack",
@@ -8,7 +10,7 @@ import re
 item_list = ["Mountain Bike"]
 
 def soup_creator(url):
-	# Downloads the eBay page for processing
+    # Downloads the eBay page for processing
     res = requests.get(url)
     # Raises an exception error if there's an error downloading the website
     res.raise_for_status()
@@ -39,7 +41,25 @@ def google_search(item):
     urls = [url for url in urls if "ebay.com" not in url] 
     urls = [url for url in urls if 'google.com' not in url] 
     urls = [url for url in urls if 'webcache' not in url] 
-    return urls
+
+    valid_urls = []
+    x = 0
+    for url in urls:
+        if(x == 4):
+            break
+        if (url.find('/dp/') != -1): # amazon product links contain this string
+            valid_urls.append((url.split('&sa=U&ved='))[0])
+            x += 1
+        elif (url.find('walmart.com') != -1):
+            valid_urls.append((url.split('&sa=U&ved='))[0])
+            x += 1
+        elif (url.find('sears.com') != -1):
+            valid_urls.append(url)
+            x += 1
+        elif (url.find('target.com') != -1):
+            valid_urls.append(url)
+            x += 1
+    return valid_urls
 
 # Scrapes and returns list of in demand items listed on eBay search result page
 def in_demand_item_finder(urls):
@@ -57,7 +77,7 @@ def in_demand_item_finder(urls):
         # We stop once we find 20 "hot" items
         x = 0
         for item in items:
-            if(x == 3):
+            if(x == 4):
                 break
             if(item.find("span", {"class": "BOLD NEGATIVE"})):
                 num_sold = item.find("span", {"class": "BOLD NEGATIVE"}).get_text()
@@ -71,20 +91,49 @@ def in_demand_item_finder(urls):
                     x += 1
 
     print("In demand items found.")
+    in_demand_items = delete_repeating_items(in_demand_items)
     return in_demand_items
 
-def description_searcher(item_link):
-    soup = soup_creator(url)
-    
+def amazon_price_checker(amazon_link, ebay_price):
+    print(amazon_link)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) \
+        AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
+    }
+     # Downloads the eBay page for processing
+    res = requests.get(amazon_link, headers=headers)
+    # Raises an exception error if there's an error downloading the website
+    res.raise_for_status()
+    # Creates a BeautifulSoup object for HTML parsing
+    soup = BeautifulSoup(res.text, 'html.parser')
+    price = soup.find('a', {"href": "gp/offer"})
+    print(price)
 
-def other_retailer_checker(item_link):
-    item_title = ((item_link.lstrip("https://www.ebay.com/itm")).split('/'))[0]
-    item_title = re.sub('-', ' ', item_title)
+def walmart_price_checker(walmart_link, ebay_price):
+    soup = soup_creator(walmart_link)
+    walmart_price = soup.find("span", {"class": "price-characteristic"}).get_text()
+    return walmart_price
+
+def delete_repeating_items(item_links):
+    item_dict = dict()
+    for item_link in item_links:
+        item_title = ((item_link.lstrip("https://www.ebay.com/itm")).split('/'))[0]
+        item_dict[item_title] = item_link
+    return item_dict.values()
+
+def other_retailer_checker(item_link, ebay_price):
     # Parses the item link to determine the item title
-   
-    print(google_search(item_title))
-    #description_searcher(item_link)
-    
+    item_title = ((item_link.lstrip("https://www.ebay.com/itm")).split('/'))[0]
+    item_title = re.sub('-', ' ', item_title) 
+    other_retailer_links = google_search(item_title)
+    for alternate_retailer_link in other_retailer_links:
+        if (alternate_retailer_link.find('/dp/') != -1):
+            amazon_price_checker(alternate_retailer_link, ebay_price)
+        if (alternate_retailer_link.find('walmart') != -1):
+            walmart_price = walmart_price_checker(alternate_retailer_link, ebay_price)
+            if(walmart_price < ebay_price):
+               print("Dropshipped item found: " + '\n' + item_title)
+               print("Walmart Link: " + '\n' + alternate_retailer_link)
 
 # Opens a hot item and determines feedback score of seller
 def eBay_item_checker(hot_items):
@@ -92,22 +141,25 @@ def eBay_item_checker(hot_items):
     for item_link in hot_items:
         if(x == 3):
             break
-        soup = soup_creator(item_link)       
+        soup = soup_creator(item_link)    
         # Finds the feedback score of the seller
+        seller_name = soup.find("span", {"class": "mbg-nw"}).get_text()
+        item_price_ebay = (soup.find(attrs = {'class': 'notranslate'}).get_text()).lstrip('US $')
         seller_info = soup.find(attrs = {'class': 'mbg-l'})    
         feedback_score = seller_info.find("span", title=True)
         feedback_score = feedback_score['title'].lstrip("feedback score:")
 
-
         if((int(feedback_score) > 100) and (int(feedback_score) < 10000)):
-            other_retailer_checker(item_link)
+            other_retailer_checker(item_link, item_price_ebay)
             #add_seller(item_link)
+        print(seller_name)
         print(feedback_score)
         x += 1
 
     print("Hot items reviewed.")
     return hot_items
         
-links = make_urls(item_list)
-hot_items = in_demand_item_finder(links)
-eBay_item_checker(hot_items)
+#links = make_urls(item_list)
+#hot_items = in_demand_item_finder(links)
+#eBay_item_checker(hot_items)
+amazon_price_checker('https://www.amazon.com/Merax-Finiss-Aluminum-Mountain-Brakes/dp/B016A59WPM', 115)
